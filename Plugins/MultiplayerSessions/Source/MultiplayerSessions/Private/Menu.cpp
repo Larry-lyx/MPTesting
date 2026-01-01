01facfc30cbf83a4d09f113e45816983f2439721
Larry_Lin <3213573967@qq.com>
2026-01-01 15:00:29 +0800

    Menu Class

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu.h"
+
+void UMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	SetIsFocusable(true);
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			FInputModeUIOnly InputModeDate;
+			InputModeDate.SetWidgetToFocus(TakeWidget());
+			InputModeDate.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			PlayerController->SetInputMode(InputModeDate);
+			PlayerController->bShowMouseCursor = true;
+		}
+	}
+}
