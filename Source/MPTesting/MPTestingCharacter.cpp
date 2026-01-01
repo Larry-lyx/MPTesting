265edf693026c9bb987125b2ca20d1bb95f6437c
Larry_Lin <3213573967@qq.com>
2026-01-01 09:16:40 +0800

    Join the Session

--- Source/MPTesting/MPTestingCharacter.cpp
+++ Source/MPTesting/MPTestingCharacter.cpp
@@ -18,7 +18,8 @@
 
 AMPTestingCharacter::AMPTestingCharacter():
 	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this , &ThisClass::OnCreateSessionComplete)) ,
-	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this , &ThisClass::OnFindSessionsComplete)) 
+	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this , &ThisClass::OnFindSessionsComplete)) ,
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this , &ThisClass::OnJoinSessionComplete)) 
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
@@ -198,6 +199,8 @@ void AMPTestingCharacter::CreateGameSession()
 	SessionSettings->bShouldAdvertise = true;
 	SessionSettings->bUsesPresence = true;
 	SessionSettings->bUseLobbiesIfAvailable = true;
+
+	SessionSettings->Set(FName("MatchType") , FString("FreeForAll") , EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 	
 	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
 	
@@ -232,6 +235,12 @@ void AMPTestingCharacter::OnCreateSessionComplete(FName SessionName, bool bWasSu
 				FColor::Blue ,
 				FString::Printf(TEXT("Created Session : %s") , *SessionName.ToString()));
 		}
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			World->ServerTravel("/Game/ThirdPerson/Lobby?listen");
+		}
 	}
 	else
 	{
@@ -248,10 +257,15 @@ void AMPTestingCharacter::OnCreateSessionComplete(FName SessionName, bool bWasSu
 
 void AMPTestingCharacter::OnFindSessionsComplete(bool bWasSuccessful)
 {
+	if (!OnlineSessionInterface.IsValid()) return;
+	
 	for (auto result : SessionSearch->SearchResults)
 	{
 		FString Id = result.GetSessionIdStr();
 		FString User = result.Session.OwningUserName;
+
+		FString MatchType;
+		result.Session.SessionSettings.Get(FName("MatchType"), MatchType);
 
 		if (GEngine)
 		{
@@ -260,6 +274,47 @@ void AMPTestingCharacter::OnFindSessionsComplete(bool bWasSuccessful)
 				15.f ,
 				FColor::Cyan ,
 				FString::Printf(TEXT("Id :%s , User : %s") , *Id , *User));
+		}
+
+		if (MatchType == FString("FreeForAll"))
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(
+					-1 ,
+					15.f ,
+					FColor::Cyan ,
+					FString::Printf(TEXT("Joining Match Type : %s") , *MatchType));
+			}
+
+			OnlineSessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
+
+			const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+			OnlineSessionInterface->JoinSession(*LocalPlayer->GetPreferredUniqueNetId() , NAME_GameSession , result);
+		}
+	}
+}
+
+void AMPTestingCharacter::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (!OnlineSessionInterface.IsValid()) return;
+
+	FString Address;
+	if (OnlineSessionInterface->GetResolvedConnectString(NAME_GameSession , Address))
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(
+				-1 ,
+				15.f ,
+				FColor::Yellow ,
+				FString::Printf(TEXT("Connect String : %s") , *Address));
+		}
+
+		APlayerController* PlayerController = GetGameInstance()->GetFirstLocalPlayerController();
+		if (PlayerController)
+		{
+			PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 		}
 	}
 }
--- Source/MPTesting/MPTestingCharacter.h
+++ Source/MPTesting/MPTestingCharacter.h
@@ -116,10 +116,12 @@ protected:
 
 	void OnCreateSessionComplete(FName SessionName , bool bWasSuccessful);
 	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName , EOnJoinSessionCompleteResult::Type Result);
 
 private:
 	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
 	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
 
 	TSharedPtr<FOnlineSessionSearch> SessionSearch;
 };
